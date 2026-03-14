6e8bfccde9e73be53541d49f3624fa74b47af9f6
Karaneigi03 <xinnegiramen@gmail.com>
2026-03-14 15:47:25 +0900

    LWCの基礎処理作成
    html→onclick属性
    JS→apex処理の呼び出し、onclickの受け取り
    APEX→apexで値の定義

--- force-app/main/default/lwc/otameshi0306/otameshi0306.js
+++ force-app/main/default/lwc/otameshi0306/otameshi0306.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
+import otameshi from '@salesforce/apex/otameshiApex0311.otameshi';
 
-export default class Otameshi0306 extends LightningElement {}
+export default class Otameshi0306 extends LightningElement {
+    //let aa = otameshi();;
+
+    handleSomething(){
+        console.log('clicked');
+        //otameshi()は、Apexクラスのメソッドを呼び出すための関数です。Apexクラスのメソッドは、サーバーサイドで実行されるため、JavaScriptから直接呼び出すことはできません。そのため、otameshi()関数を使用して、Apexクラスのメソッドを呼び出す必要があります。
+        otameshi()
+
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+   
+
+
+
+
+
+
+
+}
+
+    //console.log()
+    //クラスの中では、関数の中であっても、変数を宣言する際にはletやconstを使用する必要があります。
+    //ドナルドトランプの評判は良くないです。
+}
